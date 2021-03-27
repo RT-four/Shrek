f5859e499d323b7a4683a6dacfa668b7eaee762f
Philipp <philvas13@gmail.com>
2021-03-27 15:11:35 +0300

    dfdfdf

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ initializePassport(passport);
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/public'));
 
